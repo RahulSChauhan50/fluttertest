2d8a396d1f8b8f9120344cc5128c75c7470b0c5a
RahulSChauhan50 <rahulschauhan50@gmail.com>
2022-01-09 02:00:14 +0530

    Added font library and Created a basic form with input and button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertest/pages/homepage.dart';
 import 'package:fluttertest/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,7 +17,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       themeMode: ThemeMode.light,
       darkTheme: ThemeData(brightness: Brightness.dark),
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       initialRoute: "/login",
       routes: {"/": (context) => Home(), "/login": (context) => Login()},
     );
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,12 +1,49 @@
+import 'dart:html';
+
 import "package:flutter/material.dart";
 
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Column(children: [
-        Image.asset("assets/images/index.jpg")
-      ],),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/index.jpg",
+            fit: BoxFit.cover,
+          ),
+          Text(
+            "Welcome",
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter usename", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter password", labelText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("dvdsv");
+                    },
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                  ),
+                ],
+              )),
+        ],
+      ),
     );
   }
 }
