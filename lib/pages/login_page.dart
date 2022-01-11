0513a8c5adbcea90b977a3bbec2e3ffbfe6ca8a7
RahulSChauhan50 <rahulschauhan50@gmail.com>
2022-01-11 16:59:10 +0530

    State, Input and Animations with ternary operator

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,7 +3,14 @@ import 'dart:html';
 import "package:flutter/material.dart";
 import 'package:fluttertest/Utils/Routes.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -15,8 +22,11 @@ class Login extends StatelessWidget {
               "assets/images/index.jpg",
               fit: BoxFit.cover,
             ),
+            SizedBox(
+              height: 40,
+            ),
             Text(
-              "Welcome",
+              "Welcome" + name,
               style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
             ),
             Padding(
@@ -27,6 +37,9 @@ class Login extends StatelessWidget {
                     TextFormField(
                       decoration: InputDecoration(
                           hintText: "Enter usename", labelText: "Username"),
+                      onChanged: (value) => setState(() {
+                        name = value;
+                      }),
                     ),
                     TextFormField(
                       obscureText: true,
@@ -36,15 +49,46 @@ class Login extends StatelessWidget {
                     SizedBox(
                       height: 40,
                     ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, MyRoutes.homeRoute);
-                      },
-                      child: Text("Login"),
-                      style: TextButton.styleFrom(
-                        minimumSize: Size(150, 40),
+                    InkWell(
+                      onTap: () => setState(() {
+                        changeButton = !changeButton;
+                      }),
+                      //     Navigator.pushNamed(context, MyRoutes.homeRoute),
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: changeButton ? 110 : 150,
+                        height: 40,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          shape: changeButton
+                              ? BoxShape.circle
+                              : BoxShape.rectangle,
+                          //borderRadius: BorderRadius.circular(8)),
+                        ),
+                        child: changeButton
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18),
+                              ),
                       ),
-                    ),
+                    )
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //   },
+                    //   child: Text("Login"),
+                    //   style: TextButton.styleFrom(
+                    //     minimumSize: Size(150, 40),
+                    //   ),
+                    // ),
                   ],
                 )),
           ],
